863d3b1de0c368c3321383b258494bae372082a0
Zen <sergey.pilipchak@gmail.com>
2015-12-09 23:45:26 +0200

    Added 'if' construction examples into the Flow of control

---
+++ conspects.rb
@@ -0,0 +1,22 @@
+			#Flow of control
+
+	#if
+a = true
+
+if a == true then puts "it works"
+
+##
+if a == true
+	puts "first condition"
+	puts "second condition"
+end
+
+##
+if a == false
+	puts "a is false"
+elsif a == true		# elsif without 'e' 
+	puts "a is true"
+else
+	puts "a is not true or false"
+end
+
